2daf95d732f95933fb8fd96e1d53766f92b0b482
Victor-Lis <victorlisbronzo1@gmail.com>
2026-07-08 00:03:33 -0300

    feat(api): integrate Swagger API documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,15 +3,25 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { S3Client } from '@aws-sdk/client-s3';
-import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Lang Clips API')
+    .setDescription('API para gerenciamento de vídeos com IA.')
+    .setVersion('1.0')
+    .addTag('videos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
