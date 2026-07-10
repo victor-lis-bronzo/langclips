0c06ce7bcdf18126a3615283c45da93c5f4a4bad
Victor-Lis <victorlisbronzo1@gmail.com>
2026-07-10 16:33:16 -0300

    feat(api): setup BullMQ module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bullmq';
 import { StorageModule } from './storage/storage.module';
 import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    }),
     StorageModule,
     UploadsModule,
   ],
